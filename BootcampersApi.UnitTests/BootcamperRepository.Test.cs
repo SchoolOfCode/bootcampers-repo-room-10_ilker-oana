6f41727b3769b680aa5c941cadf12c20aaeeebf8
ilker <ilkerpeker79@yahoo.com>
2021-03-09 18:42:39 +0000

    changes made by Purple Bricks

--- BootcampersApi.UnitTests/BootcamperRepository.Test.cs
+++ BootcampersApi.UnitTests/BootcamperRepository.Test.cs
@@ -9,21 +9,42 @@ namespace BootcampersApi.UnitTests
     public class BootcamperControllerTests
     {
         [Fact]
-        public async Task GetAllBootcampers_WhenCalledWithNoParams_ReturnsOkObjectResult()
+        public async Task GetAllBootcampers_WhenCalledWithNoParams_ReturnsDefaultParams()
         {
             //Arrange. Make an instance of controller
-            var FakeRepo = new FakeRepo();
+            var testData = new List<Bootcamper> { 
+                new Bootcamper { Id = 1, Name = "Ben", CatchPhrase = "test" },
+                new Bootcamper { Id = 2, Name = "Josh", CatchPhrase = "test" },
+                new Bootcamper { Id = 3, Name = "Paul", CatchPhrase = "test" }
+            };
+
+            var FakeRepo = new FakeRepo(testData);
             var controller = new BootcamperController(FakeRepo);
 
             //Act. Call GetAllBootcampers
             var result = await controller.GetAllBootcampers(null);
 
-            //Assert. Assert.Something
-            // Assert.NotNull(controller);
-            Assert.Equal(result.StatusCode);
-            // Assert.NotNull(result);
-
+            Assert.Equal(3, result.Count);
         }
 
+        [Fact]
+        public async Task GetAllBootcampers_WhenCalledWithParam_Returns()
+        {
+            //Arrange. Make an instance of controller
+            var testData = new List<Bootcamper> { 
+                new Bootcamper { Id = 1, Name = "Ben", CatchPhrase = "test" },
+                new Bootcamper { Id = 2, Name = "Josh", CatchPhrase = "test" },
+                new Bootcamper { Id = 3, Name = "Josh", CatchPhrase = "This is also a test" },
+                new Bootcamper { Id = 4, Name = "Paul", CatchPhrase = "test" }
+            };
+
+            var FakeRepo = new FakeRepo(testData);
+            var controller = new BootcamperController(FakeRepo);
+
+            //Act. Call GetAllBootcampers
+            var result = await controller.GetAllBootcampers("Josh");
+
+            Assert.Equal(2, result.Count);
+        }
     }
 }
--- BootcampersApi.UnitTests/FakeRepo.cs
+++ BootcampersApi.UnitTests/FakeRepo.cs
@@ -1,30 +1,42 @@
+using BootcampersApi;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
- public class FakeRepo : IBootcamperRepository
+namespace BootcampersApi.UnitTests
+{
+    public class FakeRepo : IBootcamperRepository
+    {
+        List<Bootcamper> _bootcampers = new List<Bootcamper> { new Bootcamper { Id = 1, Name = "Ben", CatchPhrase = "test" } };
+
+        public FakeRepo(List<Bootcamper> initData)
         {
-            List<Bootcamper> _bootcampers = new List<Bootcamper>{new Bootcamper{Id=1, Name="Ben", CatchPhrase = "test"}};
-            
-           public async Task<IEnumerable<Bootcamper>> GetAll(int limit, int page)
-            {
-                return _bootcampers;
-            }
-            public async Task Delete(long id)
-            {
-
-            }
-            public async Task<Bootcamper> GetOne(long id)
-            {
-                return _bootcampers[0];
-            }
-            public async Task<Bootcamper> Update(Bootcamper Bootcamper)
-            {
-                return _bootcampers[0];
-            }
-            public async Task<Bootcamper> Insert(Bootcamper Bootcamper)
-            {
-                return _bootcampers[0];
-            }
-            public async Task<IEnumerable<Bootcamper>> Search(string search, int limit, int page)
-            {
-                return _bootcampers;
-            }
+            _bootcampers = initData;
         }
+
+        public async Task<IEnumerable<Bootcamper>> GetAll(int limit, int page)
+        {
+
+            return _bootcampers;
+        }
+        public async Task Delete(long id)
+        {
+
+        }
+        public async Task<Bootcamper> GetOne(long id)
+        {
+            return _bootcampers[0];
+        }
+        public async Task<Bootcamper> Update(Bootcamper Bootcamper)
+        {
+            return _bootcampers[0];
+        }
+        public async Task<Bootcamper> Insert(Bootcamper Bootcamper)
+        {
+            return _bootcampers[0];
+        }
+        public async Task<IEnumerable<Bootcamper>> Search(string search, int limit, int page)
+        {
+            return _bootcampers.FindAll(s => s.Name.Contains(search));
+        }
+    }
+}
